dc5044ef15a27b43d55d9c9b9fa321e905428b64
Konstantin Kharlin <y0.soap@gmail.com>
2011-09-24 17:47:12 +0400

    First git commit

---
+++ src/com/kharlin/testse/CollectionReturnInterface.java
@@ -0,0 +1,14 @@
+package com.kharlin.testse;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: y0
+ * Date: 24.09.11
+ * Time: 17:11
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CollectionReturnInterface<E> {
+    Collection<E> getElementsList ();
+}
---
+++ src/com/kharlin/testse/Main.java
@@ -0,0 +1,26 @@
+package com.kharlin.testse;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: y0
+ * Date: 24.09.11
+ * Time: 17:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("test");
+
+
+    }
+}
+
+class CollectionTest<String> implements CollectionReturnInterface {
+
+    public Collection<java.lang.String> getElementsList() {
+        return new ArrayList<java.lang.String>();
+    }
+}
